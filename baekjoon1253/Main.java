0862c8f741fe625400ad5f8b72894b0c00ee11f6
making-a-scene <4936joo@naver.com>
2025-01-11 20:19:50 +0900

    1253 좋다 코드 수정

--- baekjoon1253/Main.java
+++ baekjoon1253/Main.java
@@ -5,46 +5,40 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long[] arr = new long[N];
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
-        }
-        if (N < 3) {
-            System.out.println(0);
-            return;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-        Set<Long> cache = new HashSet<>();
         int count = 0;
-        if (arr[0] == arr[1] + arr[2]) {
-            count++;
-        }
-        if (arr[1] == arr[0] + arr[2]) {
-            count++;
-        }
-        for (int i = 2; i < N; i++) {
-            boolean flag = false;
+        for (int i = 0; i < N; i++) {
+            int left = 0;
+            int right = N - 1;
+            while (true) {
+                if (left == i) {
+                    left++;
+                }
+                if (right == i) {
+                    right--;
+                }
 
-            if (cache.contains(arr[i])) {
-                flag = true;
-            }
+                if (left >= right) {
+                    break;
+                }
 
-            int j = i - 2;
-            for (; j >= 0 && arr[i - 1] + arr[j] > arr[i]; j--) {
-                cache.add(arr[i] + arr[j]);
-            }
-            if (j >= 0 && arr[i - 1] + arr[j] == arr[i]) {
-                cache.add(arr[i - 1] + arr[j]);
-                flag = true;
-            }
-
-            if (flag) {
-                count++;
+                if (arr[left] + arr[right] < arr[i]) {
+                    left++;
+                } else if (arr[left] + arr[right] > arr[i]) {
+                    right--;
+                } else {
+                    count++;
+                    break;
+                }
             }
         }
-        
+
         System.out.println(count);
     }
 }
