9dc6aa3603de44818e085b7c863a21a1d5396a40
making-a-scene <4936joo@naver.com>
2025-08-17 22:13:48 +0900

    백준 11049 행렬 제곱 순서 최적화

--- baekjoon11049/Main.java
+++ baekjoon11049/Main.java
@@ -4,16 +4,16 @@ import java.util.*;
 class Main {
     static int N;
     static int[][] dp;
-    static List<int[]> arr = new ArrayList<>();
+    static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         dp = new int[N][N];
+        arr = new int[N][2];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            arr.add(new int[] {r, c});
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
         if (N == 1) {
             System.out.println(0);
@@ -24,15 +24,15 @@ class Main {
 
     private static int dynamicProgramming(int start, int end) {
         if (start + 1 == end) {
-            return arr.get(start)[0] * arr.get(start)[1] * arr.get(end)[1];
+            return arr[start][0] * arr[start][1] * arr[end][1];
         }
 
         if (dp[start][end] == 0) {
-            dp[start][end] = dynamicProgramming(start + 1, end) + arr.get(start)[0] * arr.get(start)[1] * arr.get(end)[1];
+            dp[start][end] = dynamicProgramming(start + 1, end) + arr[start][0] * arr[start][1] * arr[end][1];
             for (int i = start + 1; i < end - 1; i++) {
-                dp[start][end] = Math.min(dp[start][end], dynamicProgramming(start, i) + dynamicProgramming(i + 1, end) + arr.get(start)[0] * arr.get(i)[1] * arr.get(end)[1]);
+                dp[start][end] = Math.min(dp[start][end], dynamicProgramming(start, i) + dynamicProgramming(i + 1, end) + arr[start][0] * arr[i][1] * arr[end][1]);
             }
-            dp[start][end] = Math.min(dp[start][end], dynamicProgramming(start, end - 1) + arr.get(start)[0] * arr.get(end)[0] * arr.get(end)[1]);
+            dp[start][end] = Math.min(dp[start][end], dynamicProgramming(start, end - 1) + arr[start][0] * arr[end][0] * arr[end][1]);
         }
         return dp[start][end];
     }
